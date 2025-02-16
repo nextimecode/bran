4f7605506d3d6dbc711db5b5fadc6f2b5c8818c7
Pedro Duarte <pedrohlduarte@gmail.com>
2025-02-16 18:54:52 -0300

    feat: update server port configuration to use EnvService for better environment management

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -14,9 +15,9 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, documentFactory)
 
-  // const configService = app.get(EnvService)
-  // const port = configService.get('PORT')
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
 
-  await app.listen(process.env.PORT || 4000)
+  await app.listen(port || 3333)
 }
 bootstrap()
